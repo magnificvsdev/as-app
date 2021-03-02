9c21494568e4464b0fe1a1a861bbcacfefb73a0d
magnificvsdev <magnificvsdev@gmail.com>
2021-03-01 23:03:04 -0400

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,18 @@
+pipeline {
+    agent {
+        docker {
+            image 'node:14.16.0-alpine'
+            args '-p 3001:3001'
+        }
+    }
+    environment {
+        CI = 'true'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'npm install'
+            }
+        }
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import logoAS from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logoAS} className="App-logo" alt="logo" />
+        <p>
+        🚀Something Amazing Is Coming Soon🚀
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
